f232c8a795a1cbd69ac404c62384794865f85846
aepno <olegmernin@gmail.com>
2026-04-18 05:23:24 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+//import { useState, useEffect } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 //import { AnimatePresence } from 'framer-motion';
@@ -11,8 +11,8 @@ import { router } from './routes';
 import { queryClient } from './queryClient';
 
 export default function App() {
-  const [setLoading] = useState(true);
-/*  const [loading, setLoading] = useState(true);
+/*  const [setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => setLoading(false), 1000);
